e6f8a1ded2cd685e429e3ab9932355f9b867fd63
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-08-18 14:32:12 +0700

    Create variable.json

--- app.ts
+++ app.ts
@@ -5,12 +5,13 @@ import { DataRouter } from './src/routes/DataRouter'
 import { UserRouter } from './src/routes/UserRouter'
 
 const app = express()
+const variable = require('./public/variable.json')
 dotenv.config()
 
 app.use(bodyParser.json())
 app.use('/user', UserRouter)
 app.use('/data', DataRouter)
 
-app.listen(process.env.PORT, () => {
-    console.log('Server is running on port ' + process.env.PORT)
+app.listen((process.env.PORT ?? variable.PORT), () => {
+    console.log('Server is running on port ' + (process.env.PORT ?? variable.PORT))
 })
--- db.ts
+++ db.ts
@@ -1,10 +1,12 @@
 import * as dotenv from 'dotenv'
 import mysql from 'mysql2'
+
 dotenv.config()
+const variable = require('./public/variable.json')
 
 export const db = mysql.createConnection({
-    host: 'localhost' ?? process.env.DB_HOST,
-    user: 'root' ?? process.env.DB_USER,
-    password: '' ?? process.env.DB_PWD,
-    database: 'akuntansi' ?? process.env.DB_NAME
+    host: (process.env.DB_HOST ?? variable.DB_HOST),
+    user: (process.env.DB_USER ?? variable.DB_USER),
+    password: (process.env.DB_PWD ?? variable.DB_PWD),
+    database: (process.env.DB_NAME ?? variable.DB_NAME)
 })
