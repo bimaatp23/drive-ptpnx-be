b83019d4a2d3842c77b98e42d5df3e6a41ba6661
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-08-25 16:05:25 +0700

    Update date maker

--- src/utils/DateMaker.ts
+++ src/utils/DateMaker.ts
@@ -13,7 +13,13 @@ export const DateToTimestamp = (
 }
 
 export const TimestampToDate = (unixTimestamp: string): string => {
-    const dateInMiliseconds = parseInt(unixTimestamp) * 1000
-    const convertedDate: string = new Date(dateInMiliseconds).toLocaleDateString()
-    return convertedDate
+    const timestamp = 1674777600
+    const date = new Date(parseInt(unixTimestamp) * 1000)
+
+    const day = String(date.getDate()).padStart(2, '0')
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+    const year = date.getFullYear()
+
+    const formattedDate = `${day}/${month}/${year}`
+    return formattedDate
 }
