73a0014c591504ae465b058aed65b7babcfc9b19
Bima Tribuana Putra <bimaatp.23@gmail.com>
2023-12-31 12:27:55 +0700

    Update category service

--- src/models/CategoryModel.ts
+++ src/models/CategoryModel.ts
@@ -1,10 +1,9 @@
 import mysql, { Connection, RowDataPacket } from "mysql2"
 import { dbConfig } from "../../db"
 import { JWTRequest } from "../types/JWTRequest"
-import { Category } from "../types/category/Category"
 import { CreateCategoryReq } from "../types/category/CreateCategoryReq"
 import { DeleteCategoryReq } from "../types/category/DeleteCategoryReq"
-import { GetCategorysResp } from "../types/category/GetCategorysResp"
+import { GetCategorys, GetCategorysResp } from "../types/category/GetCategorysResp"
 import { UpdateCategoryReq } from "../types/category/UpdateCategoryReq"
 import { baseResp, conflictResp } from "../utils/Response"
 import { generateUUID } from "../utils/UUID"
@@ -12,15 +11,27 @@ import { generateUUID } from "../utils/UUID"
 export const getCategorys = (req: JWTRequest, callback: Function) => {
     const db: Connection = mysql.createConnection(dbConfig)
     db.query(
-        "SELECT * FROM category ORDER BY name ASC",
+        `SELECT
+            c.id AS category_id,
+            c.name AS category_name,
+            COUNT(d.id) AS usage_count
+        FROM
+            category c
+            LEFT JOIN
+            data d ON c.id = d.category_id
+        GROUP BY
+            c.id, c.name
+        ORDER BY
+            category_name ASC`,
         (err, result) => {
             if (err) callback(err)
             else {
                 const row = (<RowDataPacket[]>result)
-                let categorys: Category[] = row.map((data) => {
+                let categorys: GetCategorys[] = row.map((data) => {
                     return {
-                        id: data.id,
-                        name: data.name
+                        id: data.category_id,
+                        name: data.category_name,
+                        usageCount: data.usage_count
                     }
                 })
                 callback(null, baseResp(200, "Get Category List Success", categorys) as GetCategorysResp)
@@ -70,23 +81,39 @@ export const update = (req: JWTRequest, callback: Function) => {
         id: req.params.id
     }
     db.query(
-        "SELECT * FROM category WHERE name = ? AND id != ?",
-        [updateCategoryReq.name, updateCategoryReq.id],
+        "SELECT * FROM data WHERE category_id = ?",
+        [updateCategoryReq.id],
         (err, result) => {
             if (err) callback(err)
             else {
                 const row = (<RowDataPacket[]>result)
                 if (row.length > 0) {
-                    callback(null, conflictResp("Category Name Already Exists"))
+                    callback(null, conflictResp("Category Already Used"))
                 } else {
                     const db2: Connection = mysql.createConnection(dbConfig)
                     db2.query(
-                        "UPDATE category SET name = ? WHERE id = ?",
+                        "SELECT * FROM category WHERE name = ? AND id != ?",
                         [updateCategoryReq.name, updateCategoryReq.id],
                         (err, result) => {
                             if (err) callback(err)
                             else {
-                                callback(null, baseResp(200, "Update Category Success"))
+                                const row = (<RowDataPacket[]>result)
+                                if (row.length > 0) {
+                                    callback(null, conflictResp("Category Name Already Exists"))
+                                } else {
+                                    const db3: Connection = mysql.createConnection(dbConfig)
+                                    db3.query(
+                                        "UPDATE category SET name = ? WHERE id = ?",
+                                        [updateCategoryReq.name, updateCategoryReq.id],
+                                        (err, result) => {
+                                            if (err) callback(err)
+                                            else {
+                                                callback(null, baseResp(200, "Update Category Success"))
+                                            }
+                                            db3.end()
+                                        }
+                                    )
+                                }
                             }
                             db2.end()
                         }
